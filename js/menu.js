8f6567587b09747c4f579624b875d64a09733345
vadym <mirvald.vadim@icloud.com>
2022-12-16 23:39:34 +0200

    Rename title

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-const burgerIcon = document.getElementsByClassName("burger-icon");
-const navigation = document.getElementsByClassName("navigation");
-
-burgerIcon.addEventListener("click", function () {
-	navigation.classList.toggle("active");
-});
