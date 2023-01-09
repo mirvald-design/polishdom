1561dffde402a7f40240d133dc7a8c7f7ff8c548
vadym <mirvald.vadim@icloud.com>
2023-01-09 17:29:04 +0200

    new contactform

--- js/form.js
+++ js/form.js
@@ -9,6 +9,57 @@ var btn = document.getElementById("form-button");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// Get the modal
+var modal = document.getElementById("form-modal");
+
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+// Get the modal
+var modal = document.getElementById("form-modal");
+
+// When the page loads, check the state of the modal in the cookie
+if (getCookie("modalState") === "open") {
+	modal.style.display
+	"block"
+} else {
+	modal.style.display = "none"
+}
+
+// When the user closes the modal, set the state in the cookie to "closed"
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+		setCookie("modalState", "closed", 365);
+	}
+}
+
+// When the user opens the modal, set the state in the cookie to "open"
+function openModal() {
+	modal.style.display = "block";
+	setCookie("modalState", "open", 365);
+}
+
 // When the user clicks the button, open the modal
 btn.onclick = function () {
 	modal.style.display = "block";
