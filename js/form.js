4a4a13729b7839b4a43213fd1839d916749b28ca
vadym <mirvald.vadim@icloud.com>
2023-01-10 21:49:18 +0200

    add form

--- js/form.js
+++ js/form.js
@@ -1,5 +1,14 @@
 // Get the form element
 const form = document.getElementById("contact-form");
+const forf = document.getElementById("header");
+
+function openModal() {
+    document.getElementById("form-modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("form-modal").style.display = "none";
+}
 
 // Add a submit event listener to the form
 form.addEventListener("submit", function (event) {
